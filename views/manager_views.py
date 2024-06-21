7ab3c4a008bf9b88a6226abb2b0262ba03eb51cc
Kidcreole2 <131256373+Kidcreole2@users.noreply.github.com>
2024-06-21 20:34:00 +0500

    refactoring

--- views/manager_views.py
+++ views/manager_views.py
@@ -1,6 +1,7 @@
 from core import app
 from flask import render_template, request, redirect,jsonify
 from flask_login import login_required,current_user
+from flask_sqlalchemy import or_
 from models import *
 from datetime import datetime as date
 
@@ -13,12 +14,12 @@ def init_manager_views():
             Order.status == "in processing"
         ).all()
         orders_closed = Order.query.filter(
-            Order.status == "delivered", Order.status == "deprecated"
+            or_(Order.status == "delivered", Order.status == "deprecated")
         ).all()
         orders_others = Order.query.filter(
-            Order.status != "in processing",
+            or_(Order.status != "in processing",
             Order.status != "deprecated",
-            Order.status != "delivered",
+            Order.status != "delivered",)
         ).all()
         return render_template(
             "manager/index.html",
@@ -33,55 +34,19 @@ def init_manager_views():
         goods = Goods.query.all()
         return render_template("manager/goods-page.html", goods=goods)
     
-    @app.route("/<string:entity>/create", methods=["GET", "POST"])
+    @app.route("/goods/create", methods=["GET", "POST"])
     @login_required
     def create_entity(entity):
-        match entity:
-            case "order":
-                if request.method == "POST":
-                    order_json = request.form["cart"]
-                    goodses = json.loads(order_json)
-                    print(goodses)
-                    for goods in goodses:
-                        print(goods["ingridients"])
-                    new_order = Order(
-                        delivery_from_address=request.form["addressFrom"],
-                        delivery_to_address=request.form["addressTo"],
-                        delivery_date=date.now(),
-                        status="in processing",
-                        comment=request.form["comment"],
-                        price=request.form["price"],
-                    )
-                    new_order_id = Order.create(new_order)
-                    new_user_order = User_Order(
-                        user_id=current_user.id, order_id=new_order_id
-                    )
-                    User_Order.create(new_user_order)
-                    for goods in goodses:
-                        new_goods_order = Goods_Order(
-                            goods_id=goods["goods"], order_id=new_order_id
-                        )
-                        new_goods_order_id = Goods_Order.create(new_goods_order)
-                        ingridients = goods["ingridients"]
-                        for ingridient in ingridients:
-                            new_ingridient_order = Ingridient_Order(
-                                ingridient_id=ingridient, goods_order_id=new_goods_order_id
-                            )
-                            Ingridient_Order.create(new_ingridient_order)
-                    return jsonify({"message": "Заказ был успешно создан"}), 200
-                kitchens = User.query.filter(User.role == "kitchen").all()
-                return render_template("make-order.html", kitchens=kitchens)
-            case "goods":
-                if request.method == "POST":
-                    new_goods = Goods(
-                        title=request.form["title"],
-                        photo_URL=request.form["image-url"],
-                        description=request.form["description"],
-                        price=request.form["price"],
-                    )
-                    Goods.create(new_goods)
-                    return redirect("/manager/goods")
-                return render_template("manager/good-form.html")
+        if request.method == "POST":
+            new_goods = Goods(
+                title=request.form["title"],
+                photo_URL=request.form["image-url"],
+                description=request.form["description"],
+                price=request.form["price"],
+            )
+            Goods.create(new_goods)
+            return redirect("/manager/goods")
+        return render_template("manager/good-form.html")
 
 
     @app.route("/ingridient/<int:goods_id>/create", methods=["GET", "POST"])
